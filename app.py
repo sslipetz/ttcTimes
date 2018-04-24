8bc0fe0be382c21dc7f8b401acf5cca11e54fa7d
shane.s <shane.s@ecobee.com>
2018-04-24 11:28:58 -0400

    strip routes of A,B,C,etc. branches

--- app.py
+++ app.py
@@ -13,12 +13,11 @@ stops = [stop_509east,stop_511north,stop_511south,stop_510north,stop_121east]
 dirTags = {"511_1_511":"511 North- Bathurst @ Fort York",
            "511_0_511":"511 South- Bathurst @ Fort York",
            "509_0_509":"509 East- Queens Quay @ Dan Leckie",
-           "121_0_121A":"121 East- Fort York @ Bathurst",
-           "121_0_121B":"121 East- Fort York @ Bathurst",
-           "510_1_510A": "510 North- Spadina @ Fort York",
-           "510_1_510B": "510 North- Spadina @ Fort York"}
-timeList= {"511_1_511":"","511_0_511":"","509_0_509":"","121_0_121A":"","510_1_510A":""}
-keyList = ["511_1_511","511_0_511","509_0_509","121_0_121A","510_1_510A"]
+           "121_0_121":"121 East- Fort York @ Bathurst",
+           "510_1_510": "510 North- Spadina @ Fort York"}
+timeList= {"511_1_511":"","511_0_511":"","509_0_509":"","121_0_121":"","510_1_510":""}
+keyList = ["511_1_511","511_0_511","509_0_509","121_0_121","510_1_510"]
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -39,15 +38,14 @@ def index():
                                        str(int(int(vehicle['@seconds']) % 60)),"%M:%S")
             arrtimes.append(arr.strftime("%M:%S"))
 
-        dirTag = vehicle['@dirTag']
-        timeList[dirTag] = arrtimes
+        dirTag = vehicle['@dirTag'].rstrip('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        timeList[dirTag] = ", ".join(arrtimes)
+        #print(timeList)
 
     return render_template('index.html',route0=dirTags[keyList[0]],route1=dirTags[keyList[1]],
                            route2=dirTags[keyList[2]],route3=dirTags[keyList[3]],route4=dirTags[keyList[4]],
                            times0=timeList[keyList[0]],times1=timeList[keyList[1]],times2=timeList[keyList[2]],
                            times3=timeList[keyList[3]],times4=timeList[keyList[4]])
-
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
